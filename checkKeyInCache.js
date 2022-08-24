9d5b043ff01e8eb0abb74c12bd44b7f87d9d64ac
Howard <howardshen2306@gmail.com>
2022-08-24 22:26:35 +0800

    feat: add log

--- checkKeyInCache.js
+++ checkKeyInCache.js
@@ -5,13 +5,17 @@ const { redis } = require('./db/cache');
 
 const checkKeyNum = async () => {
   const keyRemain = await redis.scard(KEY_NAME);
+  console.log('Keys number in redis:', keyRemain);
   return keyRemain;
 };
 
 const main = async () => {
   const keyNum = await checkKeyNum();
   if (keyNum < KEY_THRESHOLD) {
+    console.log('Insert keys to redis');
     await insertCache();
+  } else {
+    console.log('Keys are still enough in redis');
   }
   process.exit(0);
 };
@@ -23,8 +27,7 @@ async function insertCache() {
   console.log('START TRANSACTION');
   try {
     await connection.query('START TRANSACTION');
-    const selectSql =
-      'SELECT random_key FROM url_keys WHERE is_use = 0 limit ? FOR UPDATE';
+    const selectSql = 'SELECT random_key FROM url_keys WHERE is_use = 0 limit ? FOR UPDATE';
     const updateSql =
       'UPDATE url_keys SET is_use = 1 WHERE random_key in (SELECT random_key FROM (SELECT random_key FROM url_keys WHERE is_use = 0 limit ?) as t)';
     const [encode] = await pool.execute(selectSql, [SEND_KEY_NUM]);
