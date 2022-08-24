5336234ebfd668ce205e62986390d140eeb7b952
kai5363 <57265307+kai5363@users.noreply.github.com>
2022-08-24 10:36:24 +0800

    feat: insert key and get key from db

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 require('dotenv').config();
 const { PORT } = process.env;
 const urlRoutes = require('./routes/url');
+const urlRoutesV2 = require('./routes/url_v2');
 
+app.use(express.json());
 app.use(`/api/1.0/url`, urlRoutes);
+app.use(`/api/2.0/url`, urlRoutesV2);
 
 app.listen(3000, () => {
   console.log(`server is listen on port ${process.env.PORT}....`);
---
+++ routes/url_v2.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { KEY_NAME } = process.env;
+const { redis } = require('../db/cache');
+const { checkKeyNum } = require('../util/util');
+const db = require('../db/mysql');
+const router = express.Router();
+
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { shortUrl } = req.body;
+    const longUrl = await db.execute(
+      `SELECT long_url FROM url_table WHERE short_url = ?`,
+      shortUrl
+    );
+    res.status(200).json({ longUrl });
+  })
+);
+
+router.post('/', async (req, res) => {
+  const longUrl = req.body.longUrl;
+  const shortUrl = await redis.spop(KEY_NAME);
+  await db.execute(`insert into url_table(long_url, short_url) values (?, ?)`, [
+    longUrl,
+    shortUrl,
+  ]);
+  res.status(200).json({ shortUrl });
+});
+
+module.exports = router;
