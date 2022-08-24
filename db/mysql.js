d47c72791f80f0288a3490d669af2ac53374ec02
01ccccccc <cindy92376@gmail.com>
2022-08-24 16:50:40 +0800

    [Feat] modify get method to sharding

--- db/mysql.js
+++ db/mysql.js
@@ -4,36 +4,36 @@ require('dotenv').config();
 const md5 = require('md5');
 
 const config = {
-    db_0: {
-        host: process.env.DB0_HOST,
-        port: process.env.DB0_PORT,
-        user: process.env.DB0_USER,
-        password: process.env.DB0_PWD,
-        database: process.env.DB,
-        waitForConnections: true,
-        connectionLimit: 100,
-        // queueLimit: 0,
-    },
-    db_1: {
-        host: process.env.DB1_HOST,
-        port: process.env.DB1_PORT,
-        user: process.env.DB1_USER,
-        password: process.env.DB1_PWD,
-        database: process.env.DB,
-        waitForConnections: true,
-        connectionLimit: 100,
-        // queueLimit: 0,
-    },
-    db_2: {
-        host: process.env.DB2_HOST,
-        port: process.env.DB2_PORT,
-        user: process.env.DB2_USER,
-        password: process.env.DB2_PWD,
-        database: process.env.DB,
-        waitForConnections: true,
-        connectionLimit: 100,
-        // queueLimit: 0,
-    },
+  db_0: {
+    host: process.env.DB0_HOST,
+    port: process.env.DB0_PORT,
+    user: process.env.DB0_USER,
+    password: process.env.DB0_PWD,
+    database: process.env.DB,
+    waitForConnections: true,
+    connectionLimit: 100,
+    // queueLimit: 0,
+  },
+  db_1: {
+    host: process.env.DB1_HOST,
+    port: process.env.DB1_PORT,
+    user: process.env.DB1_USER,
+    password: process.env.DB1_PWD,
+    database: process.env.DB,
+    waitForConnections: true,
+    connectionLimit: 100,
+    // queueLimit: 0,
+  },
+  db_2: {
+    host: process.env.DB2_HOST,
+    port: process.env.DB2_PORT,
+    user: process.env.DB2_USER,
+    password: process.env.DB2_PWD,
+    database: process.env.DB,
+    waitForConnections: true,
+    connectionLimit: 100,
+    // queueLimit: 0,
+  },
 };
 
 const pool = [];
@@ -41,68 +41,86 @@ pool[0] = mysql.createPool(config.db_0);
 pool[1] = mysql.createPool(config.db_1);
 pool[2] = mysql.createPool(config.db_2);
 
-async function execute(sql, params) {
-    // check array
-    if (typeof params === 'object' && params[0]) {
-        params = params.map((param) => String(param));
-    }
+// async function execute(sql, params) {
+//   // check array
+//   if (typeof params === 'object' && params[0]) {
+//     params = params.map((param) => String(param));
+//   }
 
-    // check number
-    if (typeof params === 'number') {
-        params = String(params);
-    }
+//   // check number
+//   if (typeof params === 'number') {
+//     params = String(params);
+//   }
 
-    // check string
-    if (typeof params === 'string') {
-        params = [params];
-    }
+//   // check string
+//   if (typeof params === 'string') {
+//     params = [params];
+//   }
 
-    const [results] = await pool.execute(sql, params);
-    return results;
-}
+//   const [results] = await pool.execute(sql, params);
+//   return results;
+// }
 
 const store = async (longUrl, shortUrl) => {
-    try {
-        const insertUrl = 'INSERT INTO url_table(long_url, short_url) VALUES (?, ?)';
-        const insert = await pool.execute(insertUrl, [longUrl, shortUrl]);
-        // console.log('insert', insert);
-        return 'ok';
-    } catch (err) {
-        return err;
-    }
+  try {
+    const insertUrl =
+      'INSERT INTO url_table(long_url, short_url) VALUES (?, ?)';
+    const insert = await pool.execute(insertUrl, [longUrl, shortUrl]);
+    // console.log('insert', insert);
+    return 'ok';
+  } catch (err) {
+    return err;
+  }
 };
 
 const findUrl = async (longUrl) => {
-    const hash = md5(longUrl);
-    const shortUrl = hash.slice(0, 7);
-    const lastChar = shortUrl.slice(-1);
-    const dbNum = lastChar.charCodeAt(0) % 3;
-    // console.log(dbNum);
-    let conn = await pool[dbNum].getConnection();
+  const hash = md5(longUrl);
+  const shortUrl = hash.slice(0, 7);
+  const lastChar = shortUrl.slice(-1);
+  const dbNum = lastChar.charCodeAt(0) % 3;
+  // console.log(dbNum);
+  let conn = await pool[dbNum].getConnection();
 
-    try {
-        await conn.query('START TRANSACTION');
-        const findUrl = 'SELECT short_url from url_table WHERE long_url = ?';
-        const find = await conn.execute(findUrl, [longUrl]);
-        const short = find[0][0]?.short_url;
-        if (short === undefined) {
-            const insertUrl = 'INSERT INTO url_table(long_url, short_url) VALUES (?, ?)';
-            const insert = await conn.execute(insertUrl, [longUrl, shortUrl]);
-            await conn.commit();
-            return shortUrl;
-        }
-        await conn.commit();
-        return short;
-    } catch (err) {
-        await conn.commit();
-        await conn.query('ROLLBACK');
-        return {
-            error: err,
-            status: 400,
-        };
-    } finally {
-        await conn.release();
+  try {
+    await conn.query('START TRANSACTION');
+    const findUrl = 'SELECT short_url from url_table WHERE long_url = ?';
+    const find = await conn.execute(findUrl, [longUrl]);
+    const short = find[0][0]?.short_url;
+    if (short === undefined) {
+      const insertUrl =
+        'INSERT INTO url_table(long_url, short_url) VALUES (?, ?)';
+      const insert = await conn.execute(insertUrl, [longUrl, shortUrl]);
+      await conn.commit();
+      return shortUrl;
     }
+    await conn.commit();
+    return short;
+  } catch (err) {
+    await conn.commit();
+    await conn.query('ROLLBACK');
+    return {
+      error: err,
+      status: 400,
+    };
+  } finally {
+    await conn.release();
+  }
 };
 
-module.exports = { execute, pool, store, findUrl };
+const getLongUrl = async (shortUrl) => {
+  const lastChar = shortUrl.slice(-1);
+  const dbNum = lastChar.charCodeAt(0) % 3;
+  // console.log(dbNum);
+  try {
+    let [result] = await pool[dbNum].execute(
+      'SELECT long_url AS long_url FROM url_table WHERE short_url = ?',
+      [shortUrl]
+    );
+    return result;
+  } catch (error) {
+    console.log('model error: ', error);
+    return { 'model error': error };
+  }
+};
+
+module.exports = { pool, store, findUrl, getLongUrl };
--- routes/url.js
+++ routes/url.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const db = require('../db/mysql');
 const md5 = require('md5');
-const { getLongUrl } = require('../model/model_geturl');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    let { short_url } = req.query;
-    // console.log('short_url: ', short_url);
-    let [result] = await getLongUrl(short_url);
-    // console.log('result: ', result);
-    res.status(200).json({ data: result.long_url });
+  let { shortUrl } = req.query;
+  // console.log('shortUrl: ', shortUrl);
+  let result = await db.getLongUrl(shortUrl);
+  // console.log('result: ', result);
+  res.status(200).json({ data: result.long_url });
 });
 
 router.post('/', async (req, res) => {
-    const longUrl = req.body.url;
-    const findUrl = await db.findUrl(longUrl);
-    if (!findUrl.error) {
-        shortUrl = 'http://' + findUrl;
-        res.status(200).json({ shortUrl: shortUrl });
-    } else {
-        res.status(200).json({ error: findUrl.error });
-    }
+  const longUrl = req.body.url;
+  const findUrl = await db.findUrl(longUrl);
+  if (!findUrl.error) {
+    shortUrl = 'http://' + findUrl;
+    res.status(200).json({ shortUrl: shortUrl });
+  } else {
+    res.status(200).json({ error: findUrl.error });
+  }
 });
 
 module.exports = router;
