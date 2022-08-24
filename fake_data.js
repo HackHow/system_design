1f6ff133a1a425935e29e1d6bfb64f54d019ba8c
Howard <howardshen2306@gmail.com>
2022-08-25 02:10:32 +0800

    feat: modify output log

--- fake_data.js
+++ fake_data.js
@@ -18,8 +18,13 @@ async function generateKeys() {
   const keyCount = await calcKeysCount();
   if (keyCount < KGS_KEY_THRESHOLD) {
     await insertKeys();
-    console.log('[', currTime, ']');
-    console.log('Keys generate successful !! So far not use keys:', keyCount + Number(KGS_GEN_KEY_NUM));
+    console.log();
+    console.log(
+      '[',
+      currTime,
+      '] Keys generate successful!! So far not use keys:',
+      keyCount + Number(KGS_GEN_KEY_NUM)
+    );
     console.log('------------------------');
   } else {
     console.log('[', currTime, '] So far not use keys:', keyCount);
