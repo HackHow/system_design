2f8a819acac2dac2f1390ca9b0daf7a08b61878f
Howard <howardshen2306@gmail.com>
2022-08-24 22:09:08 +0800

    feat: add KEY_THRESHOLD

--- checkKeyInCache.js
+++ checkKeyInCache.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: __dirname + '/.env' });
-const { KEY_NAME } = process.env;
+const { KEY_NAME, KEY_THRESHOLD } = process.env;
 const { pool } = require('./db/mysql');
 const { redis } = require('./db/cache');
 
