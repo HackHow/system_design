4cb5bd4286a0c8678efe3a468300342cfc0d2584
kai5363 <57265307+kai5363@users.noreply.github.com>
2022-08-25 20:53:32 +0800

    feat: health

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ const { PORT } = process.env;
 const urlRoutes = require('./routes/url');
 
 app.use(express.json());
-app.use(`/`, (req, res) => {
-  res.status(200).send({ ok: 'ok' });
-});
 
-app.use(`/api/1.0/url`, urlRoutes);
+app.use('/api/1.0/url', urlRoutes);
 
 // Handle 404
 app.use(function (req, res, next) {
@@ -22,6 +19,10 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   console.log('error handler: ', err);
   return res.status(500).render('error', { msg: 'error: 500' });
+});
+
+app.use('/', (req, res) => {
+  res.status(200).send({ ok: 'ok' });
 });
 
 app.listen(3000, () => {
