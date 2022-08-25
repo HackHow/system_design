66f49deb846bb95e418e18d3bc69440370aeed25
kai5363 <57265307+kai5363@users.noreply.github.com>
2022-08-25 21:59:25 +0800

    feat: health

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const urlRoutesV2 = require('./routes/url_v2');
 const keyGenRoutesV2 = require('./routes/key_gen');
 
 app.use(express.json());
-app.use(`/`, (req, res) => {
-  res.status(200).send({ ok: 'ok' });
-});
+
 app.use(`/api/1.0/url`, urlRoutes);
 app.use(`/api/2.0/url`, urlRoutesV2);
 app.use(`/api/2.0/keys`, keyGenRoutesV2);
+
+app.use(`/health`, (req, res) => {
+  res.status(200).send({ ok: 'ok' });
+});
 
 app.listen(PORT, () => {
   console.log(`server is listen on port ${PORT}....`);
