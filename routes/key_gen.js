66738764b605024cef3a1b442123a3e83e0d5949
Howard <howardshen2306@gmail.com>
2022-08-25 11:12:51 +0800

    feat: add router log

--- routes/key_gen.js
+++ routes/key_gen.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { redis } = require('../db/cache');
 const { KEY_NAME, REQ_KEY_NUM } = process.env;
+const dayjs = require('dayjs');
 
 const router = express.Router();
+const currTime = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss:SSS');
 
 router.get('/', async (req, res) => {
   const key = await redis.spop(KEY_NAME, REQ_KEY_NUM);
+  console.log('[', currTime, '] Receive get keys request!!');
   res.status(200).send(key);
 });
 
