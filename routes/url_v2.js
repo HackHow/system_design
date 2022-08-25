e78ff0cb0828d22af010e47a6661aa30683afa26
kai5363 <57265307+kai5363@users.noreply.github.com>
2022-08-25 11:19:08 +0800

    feat: change db

--- routes/url_v2.js
+++ routes/url_v2.js
@@ -5,16 +5,17 @@ const axios = require('axios');
 const { pool_cluster } = require('../db/mysql');
 const router = express.Router();
 const { KGS_URL, REQ_KEY_NUM, THRESHOLD_RATIO } = process.env;
-const keyBuffer = [];
+let keyBuffer = [];
 
 router.get(
   '/',
   asyncHandler(async (req, res) => {
     const { shortUrl } = req.body;
     const mod = shortUrl.slice(-1).charCodeAt(0) % 3;
-    const [result] = await pool_cluster[mod].execute(`SELECT long_url FROM url_table WHERE short_url = ?`, [
-      shortUrl,
-    ]);
+    const [result] = await pool_cluster[mod].execute(
+      `SELECT long_url FROM url_table2 WHERE short_url = ?`,
+      [shortUrl]
+    );
     if (!result.length) {
       res.status(200).json({ message: 'not found' });
     }
@@ -27,15 +28,15 @@ router.post('/', async (req, res) => {
   const threshold = REQ_KEY_NUM * THRESHOLD_RATIO;
   if (keyBuffer.length < threshold) {
     const { data } = await axios.get(KGS_URL);
-    keyBuffer.push(data);
+    keyBuffer = [...keyBuffer, ...data];
   }
 
   const shortUrl = keyBuffer.pop();
   const mod = shortUrl.slice(-1).charCodeAt(0) % 3;
-  await pool_cluster[mod].execute(`insert into url_table(long_url, short_url) values (?, ?)`, [
-    longUrl,
-    shortUrl,
-  ]);
+  await pool_cluster[mod].execute(
+    `insert into url_table2(long_url, short_url) values (?, ?)`,
+    [longUrl, shortUrl]
+  );
   res.status(200).json({ shortUrl });
 });
 
