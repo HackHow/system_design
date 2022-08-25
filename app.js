8e91449df4fd81399c745cbce2f7e917f95ad1fb
kai5363 <57265307+kai5363@users.noreply.github.com>
2022-08-25 17:18:44 +0800

    feat: add health check route

--- app.js
+++ app.js
@@ -5,22 +5,25 @@ const { PORT } = process.env;
 const urlRoutes = require('./routes/url');
 
 app.use(express.json());
+app.use(`/`, (req, res) => {
+  res.status(200).send({ ok: 'ok' });
+});
 
 app.use(`/api/3.0/url`, urlRoutes);
 
 // Handle 404
 app.use(function (req, res, next) {
-    // console.log('req.query: ', req.query);
-    console.log('404', req.url);
-    return res.status(404).json({ error: 'error: 404' });
+  // console.log('req.query: ', req.query);
+  console.log('404', req.url);
+  return res.status(404).json({ error: 'error: 404' });
 });
 
 //Handle 500
 app.use(function (err, req, res, next) {
-    console.log('error handler: ', err);
-    return res.status(500).render('error', { msg: 'error: 500' });
+  console.log('error handler: ', err);
+  return res.status(500).render('error', { msg: 'error: 500' });
 });
 
 app.listen(3000, () => {
-    console.log(`server is listen on port ${process.env.PORT}....`);
+  console.log(`server is listen on port ${process.env.PORT}....`);
 });
